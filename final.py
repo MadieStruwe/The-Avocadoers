e5586a5d342643f9321538681e3ef04735e16233
Madelaine Struwe <44145676+MadieStruwe@users.noreply.github.com>
2018-12-03 23:13:38 -0700

    Update final.py

--- final.py
+++ final.py
@@ -1,22 +1,51 @@
-import argparse
+'''
+NOTE:
+To run for the first time, ensure it it exacutable by 
+changing the permissions by running the following in the 
+command line:
+	sudo chmod +x final.py
+To run the program use python3, run the following in
+the command line:
+	python3 ./final.py
+'''
+
 import pandas as pd
 import numpy as np
-from pathlib import Path
 import pickle
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
-import datetime
-import csv
+from pathlib import Path
+import math
 
 '''
 description of what to do:
-* first read in the files we want to use 
-* store the data of crops yeild, price sold, etc. in something like an array.
-* for every temp value we are loking at, rate it on the scale
-* store those results in an array
-* do math to that temp array to compare to the crops yeild and such
+first read in the files we want to use 
+store the data of crops yeild, price sold, etc. in something like an array.
+for every temp value we are loking at, rate it on the scale
+store those results in an array
+do math to that temp array to compare to the crops yeild and such
     perhaps look at it as a percentage, ie, crops yeild for total availble fields
-* store if the year was good or bad
+store if the year was good or bad
     potetially instead of year, use quarters, or by month
 '''
+
+def ReadInAttributeFromCSV(fileName, attribute):
+	attributeValues = []
+	f = open(fileName, 'r')
+	# Create a csv reader that stores the each column of the first line of the file
+	# as keys and then the rest of the lines of each column as values for those keys
+	reader = csv.DictReader(f)
+	for row in reader:
+		if attribute in row:
+			if attribute != 'date':
+				value = float(row[attribute])
+				attributeValues.append(value)
+			else:
+				date = datetime.datetime.strptime(row[attribute],'%Y/%m/%d')
+				attributeValues.append(date)
+	f.close()
+	return attributeValues
+
+def main():
+	print ("Hello World")
+
+if __name__ == '__main__':
+	main()
